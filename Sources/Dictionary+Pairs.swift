4e471254bd01657035e4cc4ecb70fc6aa83c01e3
Fernando Paredes <nano@fdp.io>
2017-06-01 10:24:00 -0700

    [JSON] Conform to Equatable

---
+++ Sources/Dictionary+Pairs.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension Dictionary {
+    init(_ pairs: [Element]) {
+        self.init()
+
+        for (key, value) in pairs {
+            self[key] = value
+        }
+    }
+}
