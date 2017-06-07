66d6d4d689f813720e27df51a0877a9c2404cb30
Fernando Paredes <nano@fdp.io>
2017-06-07 10:16:11 -0700

    Move Error to BourneError

--- Sources/Bourne.swift
+++ Sources/Bourne.swift
@@ -1,1 +1,6 @@
 import Foundation
+
+public enum BourneError: Error {
+    case emptyJSON(String)
+    case undecodable(String)
+}
--- Sources/DecodeError.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public enum DecodeError: Error {
-    case emptyJSON(String)
-    case undecodable(String)
-}
